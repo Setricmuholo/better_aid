a7a32317e1ebf03d0f65963140d1ba6ba487f4b2
Setricmuholo <110999550+Setricmuholo@users.noreply.github.com>
2023-09-13 13:21:26 +0300

    activated the login and register buttons

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    #thematic = db.Column(db.String(100))
 
 @app.route('/')
 def index():
@@ -10,5 +21,41 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        # Add authentication logic
+
+        flash('Login succesful', 'success')
+        return redirect(url_for('index.html'))
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        #thematic = request.form['thematic']
+        # Add more fields from the registration form
+
+        # Check if the email is already registered
+        if User.query.filter_by(email=email).first():
+            flash('Email address is already registered', 'danger')
+            return redirect(url_for('register'))
+
+        # Create a new user and store in the database
+        new_user = User(name=name, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
